7f3c8745a18fba25895cf42a0442fae66c948970
dicenull <dicenull.dev@gmail.com>
2023-11-14 21:47:34 +0900

    update

---
+++ .bin/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(uname)" != "Darwin" ] ; then
+	echo "Not macOS!"
+	exit 1
+fi
+
+
+# HomeBrew https://brew.sh/ja/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+
+# fish
+brew install fish
+
+echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
+
+chsh -s /opt/homebrew/bin/fish
---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles for Mac
+
+## 参考
+
+- <https://zenn.dev/tsukuboshi/articles/6e82aef942d9af>
