a0a083a8f8c00496d3880e66b7efbe4634aedf4d
dicenull <dicenull.dev@gmail.com>
2023-11-18 18:03:43 +0900

    add .config

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+eval (/opt/homebrew/bin/brew shellenv)
+
+alias chrome='open /Applications/Google\ Chrome.app/'
+alias trja='trans {en=ja}'
+alias tren='trans {ja=en}'
+alias lg='lazygit'
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/Users/dice/Library/Application Support/Coursier/bin"
+# <<< coursier install directory <<<
