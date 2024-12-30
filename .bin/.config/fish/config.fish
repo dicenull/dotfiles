91a638523cb1087019db201aa4a3ea06d1292756
dicenull <dicenull.dev@gmail.com>
2024-12-30 17:26:41 +0900

    update

--- .bin/.config/fish/config.fish
+++ .bin/.config/fish/config.fish
@@ -5,6 +5,6 @@ alias trja='trans {en=ja}'
 alias tren='trans {ja=en}'
 alias lg='lazygit'
 
-# >>> coursier install directory >>>
-set -gx PATH "$PATH:/Users/dice/Library/Application Support/Coursier/bin"
-# <<< coursier install directory <<<
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
